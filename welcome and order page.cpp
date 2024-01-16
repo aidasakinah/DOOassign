b2aaa1ee595a555f7226ce886d38d22dd0bfd16f
Yongxy0319 <129039219+Yongxy0319@users.noreply.github.com>
2024-01-16 23:00:40 +0800

    Update welcome and order page.cpp

--- welcome and order page.cpp
+++ welcome and order page.cpp
@@ -1,168 +1,69 @@
 #include<iostream>
-#include<string>
+#include<iomanip>
+#include<fstream>
 using namespace std; 
-//welcome
-class Food
+
+class Restaurant
 {
 	private:
-	string title;
+	float Nasilemak, Currymee, Friedrice, Meesoup, Asamlaksa, Prawnmee, Teh_hot, Teh_cold, Coffee_hot, Coffee_cold, Milo_hot, Milo_cold, Lemontea_hot, Lemontea_cold;
 	float price;
 	
 	public:
-	void displayMenu()
+	void displayMenu(int nasi, int curry, int fried, int mee, int asam, int prawn, int teh_h, int teh_c, int coffee_h, int coffee_c, int milo_h, int milo_c, int lemon_h, int lemon_c)
 	{
-		cout<<"---------------------------------------------------"<<endl;
-		cout<<"\tRestaurant Fusion Fare Delight"<<endl;
-		cout<<"---------------------------------------------------"<<endl;
-		cout<<"Food/Beverage"<<endl;
-		cout<<"1) Nasi Lemak with Fried Chicken  RM8.50"<<endl;
-		cout<<"2) Chicken Satay (10stick)  RM10.00"<<endl;
-		cout<<"3) Curry Mee  RM7.00"<<endl;
-		cout<<"4) Kampung Fried Rice  RM7.50"<<endl;
-		cout<<"5) Mee Soup  RM6.00"<<endl;
-		cout<<"6) Asam Laksa  RM8.00"<<endl;
-		cout<<"7) Prawn Mee  RM6.50"<<endl;
-		cout<<"8) Sambal Fried Rice  RM7.00"<<endl; 
-		cout<<"9) Teh (hot)  RM2.00"<<endl;
-		cout<<"10) Teh (cold)  RM2.50"<<endl;
-		cout<<"11) Coffee (hot)  RM2.30"<<endl;
-		cout<<"12) Coffee (cold)  RM2.80"<<endl;
-		cout<<"13) Milo (hot)  RM3.00"<<endl;
-		cout<<"14) Milo (cold)  RM3.50"<<endl;
-		cout<<"15) Lemon Tea (hot)  RM3.00"<<endl;
-		cout<<"16) Lemon Tea (cold)  RM3.50"<<endl;
+		Nasilemak = static_cast<float>(nasi);
+		Currymee = static_cast<float>(curry);
+ 		Friedrice = static_cast<float>(fried);
+ 		Meesoup = static_cast<float>(mee);
+ 		Asamlaksa = static_cast<float>(asam);
+ 		Prawnmee = static_cast<float>(prawn);
+ 		Teh_hot = static_cast<float>(teh_h);
+		Teh_cold = static_cast<float>(teh_c);
+		Coffee_hot = static_cast<float>(coffee_h);
+		Coffee_cold = static_cast<float>(coffee_c);
+		Milo_hot = static_cast<float>(milo_h);
+		Milo_cold = static_cast<float>(milo_c);
+		Lemontea_hot = static_cast<float>(lemon_h);
+		Lemontea_cold = static_cast<float>(lemon_c);
+		
+		getOrder();
 	}
 	
 	void getOrder()
 	{
-		int choice;
-		cout<<"Please select your item : ";
-		cin>>choice;
+		cout<<"---------------------------------------------------"<<endl;
+		cout<<"\tRestaurant Fusion Fare Delight"<<endl;
+		cout<<"---------------------------------------------------"<<endl;
+		cout<<"Nasi Lemak with Fried Chicken :"<<Nasilemak<<" bowl(s)"<<endl;
+		cout<<"Curry Mee :"<<Currymee<<" bowl(s)"<<endl;
+		cout<<"Fried Rice :"<<Friedrice<<" bowl(s)"<<endl;
+		cout<<"Mee Soup :"<<Meesoup<<" bowl(s)"<<endl;
+		cout<<"Asam Laksa :"<<Asamlaksa<<" bowl(s)"<<endl;
+		cout<<"Prawn Mee :"<<Prawnmee<<" bowl(s)"<<endl;
+		cout<<"Teh (hot) :"<<Teh_hot<<" cup(s)"<<endl;
+		cout<<"Teh (cold) :"<<Teh_cold<<" cup(s)"<<endl;
+		cout<<"Coffee (hot) :"<<Coffee_hot<<" cup(s)"<<endl;
+		cout<<"Coffee (cold) :"<<Coffee_cold<<" cup(s)"<<endl;
+		cout<<"Milo (hot) :"<<Milo_hot<<" cup(s)"<<endl;
+		cout<<"Milo (cold) :"<<Milo_cold<<" cup(s)"<<endl;
+		cout<<"Lemon Tea (hot) :"<<Lemontea_hot<<" cup(s)"<<endl;
+		cout<<"Lemon Tea (cold) :"<<Lemontea_cold<<" cup(s)"<<endl;
 		
-		if (choice == 1)
-		{
-			title = "Nasi Lemak with Fried Chicken";
-			price = 8.50;
-		}
-		else if (choice == 2)
-		{
-			title = "Chicken Satay (10stick)";
-			price = 10.00;
-		}
-		else if (choice == 3)
-		{
-			title = "Curry Mee";
-			price = 7.00;
-		}
-		else if (choice == 4)
-		{
-			title = "Kampung Fried Rice";
-			price = 7.50;
-		}
-		else if (choice == 5)
-		{
-			title = "Mee Soup";
-			price = 6.00;
-		}
-		else if (choice == 6)
-		{
-			title = "Asam Laksa";
-			price = 8.00;
-		}
-		else if (choice == 7)
-		{
-			title = "Prawn Mee";
-			price = 6.50; 
-		}
-		else if (choice == 8)
-		{
-			title = "Sambal Fried Rice";
-			price = 7.00;
-		}
-		else if (choice == 9)
-		{
-			title = "Teh (hot)";
-			price = 2.00;
-		}
-		else if (choice == 10)
-		{
-			title = "Teh (cold)";
-			price = 2.50;
-		}
-		else if (choice == 11)
-		{
-			title = "Coffee (hot)";
-			price = 2.30;
-		}
-		else if (choice == 12)
-		{
-			title = "Coffee (cold)";
-			price = 2.80;
-		}
-		else if (choice == 13)
-		{
-			title = "Milo (hot)";
-			price = 3.00;
-		}
-		else if (choice == 14)
-		{
-			title = "Milo (cold)";
-			price = 3.50;
-		}
-		else if (choice == 15)
-		{
-			title = "Lemon Tea (hot)";
-			price = 3.00;
-		}
-		else if (choice == 16)
-		{
-			title = "Lemon Tea (cold)";
-			price = 3.50;
-		}
-		else
-		{
-			title = "No title";
-			price = 0.00;
-		}
+		cout<<"Total bowls :"<< Nasilemak + Currymee + Friedrice + Meesoup + Asamlaksa + Prawnmee<<endl;
+		cout<<"Total cups :"<< Teh_hot + Teh_cold + Coffee_hot + Coffee_cold + Milo_hot + Milo_cold + Lemontea_hot + Lemontea_cold<<endl;
 	}
-	
-	double getPrice()
+		
+	void getPrice()
 	{
-		return price;
+		price = Nasilemak * 8.50 + Currymee * 7.00 + Friedrice * 7.00 + Meesoup * 6.00 + Asamlaksa * 8.00 + Prawnmee * 6.50 + Teh_hot * 2.00 + Teh_cold * 2.50 + Coffee_hot * 2.30 + Coffee_cold * 2.80 + Milo_hot * 3.00 + Milo_cold * 3.50 + Lemontea_hot * 3.00 + Lemontea_cold * 3.50;
+		cout<<"Thank you. Your order price is RM"<<price<<endl;
 	}
-	
-	string getTitle()
-	{
-		return title;
-	}
-};
-
-class Drink
-{
-	private:
-		string Menu_title;
-		double payment;
-		
-	Food F;
-		
-	public:
-		void showOrder()
-		{
-			F.displayMenu();
-			F.getOrder();
-			payment = F.getPrice();
-			Menu_title = F.getTitle();
-		}
-		
-		void details()
-		{
-			cout<<Menu_title<<", add successfully"<<endl;
-			cout<<"Price : RM"<<payment<<endl;
-		}
 };
 
 int main()
 {
+	Restaurant R;
 	//welcome page
 	cout<<"---------------------------------------------------------"<<endl;
 	cout<<"\tWelcome to Restaurant Fusion Fare Delight!"<<endl;
@@ -171,9 +72,60 @@ int main()
 	cout<<"Monday - Friday, 7am until 5pm"<<endl;
 	cout<<"Saturday - Sunday, 8am until 3pm\n"<<endl;
 	
-	Drink D;
-	D.showOrder();
-	D.details();
+	char choice;
+	ifstream inData;
+	
+	do
+	{
+		cout<<"---------------------------------------------------"<<endl;
+		cout<<"\tRestaurant Fusion Fare Delight"<<endl;
+		cout<<"---------------------------------------------------"<<endl;
+		cout<<"Looking for a food/drinks? [Y/N]\n";
+		cin>>choice;
+		
+		if (choice == 'Y' || choice == 'y')
+		{
+			int nasi, curry, fried, mee, asam, prawn, teh_h, teh_c, coffee_h, coffee_c, milo_h, milo_c, lemon_h, lemon_c;
+			cout<<"Food/Beverages"<<endl;
+			cout<<"1) Nasi Lemak with Fried Chicken (bowl) : ";
+			cin>>nasi;
+			cout<<"2) Curry Mee (bowl) : ";
+			cin>>curry;
+			cout<<"3) Fried Rice (bowl) : ";
+			cin>>fried;
+			cout<<"4) Mee Soup (bowl) : ";
+			cin>>mee;
+			cout<<"5) Asam Laksa (bowl) : ";
+			cin>>asam;
+			cout<<"6) Prawn Mee (bowl) : ";
+			cin>>prawn;
+			cout<<"7) Teh (hot) (cup) : ";
+			cin>>teh_h;
+			cout<<"8) Teh (cold) (cup) : ";
+			cin>>teh_c;
+			cout<<"9) Coffee (hot) (cup) : ";
+			cin>>coffee_h;
+			cout<<"10) Coffee (cold) (cup) : ";
+			cin>>coffee_c;
+			cout<<"11) Milo (hot) (cup) : ";
+			cin>>milo_h;
+			cout<<"12) Milo (cold) (cup) : ";
+			cin>>milo_c;
+			cout<<"13) Lemon Tea (hot) (cup) : ";
+			cin>>lemon_h;
+			cout<<"14) Lemon Tea (cold) (cup) : ";
+			cin>>lemon_c;
+			
+			R.displayMenu(nasi, curry, fried, mee, asam, prawn, teh_h, teh_c, coffee_h, coffee_c, milo_h, milo_c, lemon_h, lemon_c);
+			R.getPrice();
+			
+			cout << "Would you like to make another order? [Y/N]\n";
+            cin >> choice;
+		}
+	} while (choice == 'Y' || choice == 'y');
+	
+	inData.open("Menu.txt");
+	inData.close();
 	
 	return 0;
 }
